011255afda7d9e8c71a2131471510aedd6f3b09b
SeanQuinn <Seanquinn917@gmail.com>
2023-09-19 11:58:30 -0400

    commit tree d0e5d5741d5c9984ad4e6e89727e1c49c0e84623

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+
+
+  def initialize(a, b ,c)
+   @a = a
+   @b = b
+   @c = c
+
+  end
+
+  def kind
+    if @a == 0 || @b == 0 || @c == 0
+      raise TriangleError.new("Triangle sides must be larger than 0.")
+    elsif @a < 0 || @b < 0 || @c < 0
+      raise TriangleError.new("Triangle sides cannot be negative.")
+    elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+      raise TriangleError.new("Triangle violates the triangle inequality.")
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    elsif @a != @b && @a != @c && @b != @c
+      :scalene
+    end
+  end
+
+    class TriangleError < StandardError
+      def initialize(message)
+        super(message)
+    end
+  end
+
+  
+
 end
